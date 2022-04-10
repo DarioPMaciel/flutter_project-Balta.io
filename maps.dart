730910e7dda8be9f2059ffd8a3379cdddcf9cdb3
dario <dariodepaulamaciel@hotmail.com>
2022-04-10 11:04:28 -0300

    Maps

---
+++ maps.dart
@@ -0,0 +1,12 @@
+main() {
+  // ignore: unused_local_variable
+  var maps = {
+    "nome": "Jacob Moura",
+    "idade": 38,
+    "cidade": "Maringa",
+    "estado": "PR"
+  };
+  print(maps["nome"]);
+  maps["nome"] = "José da Silva";
+  print(maps["nome"]);
+}
