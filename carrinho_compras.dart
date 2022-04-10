9fab7d2db743dfc49df4c77b872eda2d3d7849b9
dario <dariodepaulamaciel@hotmail.com>
2022-04-10 09:32:10 -0300

    List

--- carrinho_compras.dart
+++ carrinho_compras.dart
@@ -5,18 +5,26 @@ main() {
   bool condicao = true;
 
   while (condicao) {
-    print("ADICIONE SEUS PRODUTOS");
+    print("===ADICIONE SEUS PRODUTOS===");
     String? text = stdin.readLineSync();
     if (text == 'sair') {
       print("=== Terminou o Programa===");
       condicao = false;
     } else if (text == 'imprimir') {
-      print(produtos);
-      print("\n");
+      for (var i = 0; i < produtos.length; i++) {
+        print("ITEM $i - ${produtos[i]}");
+      }
+    } else if (text == 'remover') {
+      print("===Qual produto Remover===");
+      for (var i = 0; i < produtos.length; i++) {
+        print("ITEM $i - ${produtos[i]}");
+      }
+      int item = int.parse(stdin.readLineSync()!);
+      produtos.removeAt(item);
+      print("======ITEM REMOVIDO");
     } else {
       produtos.add(text!);
-      print("\x1B[2J\x1B[0;0H"); //comando para limpar a tela após item
-
+      print("\x1B[2J\x1B[0;0H"); //comando para limpar a tela a
     }
   }
 }
