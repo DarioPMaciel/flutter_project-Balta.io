6cd3093c8612587373bf68c609383bbd9e1597c4
dario <dariodepaulamaciel@hotmail.com>
2022-04-10 07:52:17 -0300

    baltaio

---
+++ Arrays.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+main() {
+  /*String nome = "Dario";
+  nome = "Bia";*/
+/*
+  var nome = [];
+
+  bool condicao = true;
+
+  while (condicao) {
+    print("Digite um Nome: ");
+    String? text = stdin.readLineSync();
+    if (text == "sair") {
+      print("=====PROGRAMA FINALIZADO=====");
+      condicao = false;
+    } else {
+      nome.add(text);
+    }
+    print(nome);
+    print('\n');
+  }*/
+}
